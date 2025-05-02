ef5c4f3c3603eee8fe1ba886ef677510f174e450
Fahmi <fahmianton@gmail.com>
2025-05-02 16:25:00 +0700

    Remove files that are now ignored by .gitignore

--- reward-system_mini-project/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-database_1.sequelize.sync({ force: false }).then(() => {
-    console.log('Database synced successfully!');
-}).catch((error) => {
-    console.error('Error syncing the database:', error);
-});
